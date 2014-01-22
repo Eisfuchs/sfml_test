4c8ff5e90d6f890d455b3c93840bbc22cdff53a6
Eisfuchs <larslanger87@gmail.com>
2014-01-22 11:33:25 +0100

    add movement depending of framerate and add display text

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
-//#include <SFML/System.hpp>
+#include <SFML/System.hpp>
 //#include <SFML/Network.hpp>
-//#include <SFML/Window.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
@@ -14,12 +14,20 @@ int main()
 		return EXIT_FAILURE;
 	sf::Sprite sprite(texture);
 	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile("arial.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("Hello SFML", font, 50);
 	// Load a music to play
 	sf::Music music;
 	if (!music.openFromFile("mario.ogg"))
 		return EXIT_FAILURE;
 	// Play the music
 	music.play();
+	
+	// starts the clock
+	sf::Clock clock; 
+	
 	// Start the game loop
 	while (window.isOpen())
 	{
@@ -31,14 +39,16 @@ int main()
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+
+		sf::Time elapsedTime = clock.getElapsedTime();
+		clock.restart();
 		
-		double speed = 0.1;
+		double speed = 0.0001 * elapsedTime.asMicroseconds();
 
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 		{
 			sprite.setPosition(sprite.getPosition().x, sprite.getPosition().y - speed);
 		}
-
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
 		{
 			sprite.setPosition(sprite.getPosition().x, sprite.getPosition().y + speed);
@@ -57,7 +67,7 @@ int main()
 		// Draw the sprite
 		window.draw(sprite);
 		// Draw the string
-		//window.draw(text);
+		window.draw(text);
 		// Update the window
 		window.display();
 	}
